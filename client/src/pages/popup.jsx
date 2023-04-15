cbec199a53ec83abbfb3321de579f75e4c666203
Daniel Li <xwingpilot15@gmail.com>
2023-04-15 15:09:52 -0500

    crap

--- client/src/pages/popup.jsx
+++ client/src/pages/popup.jsx
@@ -1,11 +1,8 @@
 //temporary until anmol finishes todolist
-import React from 'react';
-import Popup from 'reactjs-popup';
-import 'reactjs-popup/dist/index.css';
-
-export default function Popup() {
+export default function PopupGfg() {
     return (
         <div>
+            <h4>Popup - GeeksforGeeks</h4>
             <Popup trigger=
                 {<button> Click to open modal </button>}
                 modal nested>
@@ -13,12 +10,12 @@ export default function Popup() {
                     close => (
                         <div className='modal'>
                             <div className='content'>
-                                Nyaa, you didn't finish in time! I'm confiscating five coins until you finish another task on time.
+                                Welcome to GFG!!!
                             </div>
                             <div>
                                 <button onClick=
                                     {() => close()}>
-                                        Close
+                                        Close modal
                                 </button>
                             </div>
                         </div>
