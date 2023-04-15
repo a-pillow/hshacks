36731510e702d97d4e301b5887357eb56c6cdcd2
a-pillow <kaluhblee@gmail.com>
2023-04-15 10:41:08 -0500

    navbar update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,10 +6,21 @@ export default function Navbar() {
             <h1 className="text-3xl font-bold border-2 border-brand-primary px-2 py-1 rounded-lg">
                 pawsitive
             </h1>
-            <div>
-                Hello
+            <div className="">
+                <a className="px-4 py-1 mx-2 hover:border-brand-primary border-b-2 border-brand-background duration-150 cursor-pointer ">
+                    Hello
+                </a>
+                <a className="px-4 py-1 mx-2 hover:border-brand-primary border-b-2 border-brand-background duration-150 cursor-pointer ">
+                    Hello
+                </a>
+                <a className="px-4 py-1 mx-2 hover:border-brand-primary border-b-2 border-brand-background duration-150 cursor-pointer ">
+                    Hello
+                </a>
+                <a className="px-4 py-1 mx-2 hover:border-brand-primary border-b-2 border-brand-background duration-150 cursor-pointer ">
+                    Hello
+                </a>
             </div>
-            <button className="text-lg font-bold border-2 bg-brand-primary px-2 py-1 rounded-lg text-brand-background">
+            <button className="text-lg font-bold border-2 bg-brand-primary px-4 py-1 rounded-lg hover:bg-brand-background hover:border-brand-background hover:-translate-y-[2px] hover:drop-shadow-lg hover:text-brand-primary duration-150  text-brand-background">
                 About 
             </button>
         </nav>
