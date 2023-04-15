cca9182d1f4a5c4f6ba3be75f83fa07a5e371c52
a-pillow <kaluhblee@gmail.com>
2023-04-15 10:47:43 -0500

    sex plzzz

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,22 +2,19 @@
 export default function Navbar() {
 
     return (
-        <nav className="w-full py-2 px-12 flex justify-between items-center">
-            <h1 className="text-3xl font-bold border-2 border-brand-primary px-2 py-1 rounded-lg">
+        <nav className="w-full py-2 px-12 mb-6 flex justify-between items-center">
+            <h1 className="text-3xl font-bold border-2 border-brand-primary px-2 py-1 cursor-pointer hover:bg-brand-primary hover:text-brand-background hover:border-brand-primary hover:-translate-y-[2px] hover:drop-shadow-lg duration-150 rounded-lg">
                 pawsitive
             </h1>
             <div className="">
                 <a className="px-4 py-1 mx-2 hover:border-brand-primary border-b-2 border-brand-background duration-150 cursor-pointer ">
-                    Hello
+                    Home
                 </a>
                 <a className="px-4 py-1 mx-2 hover:border-brand-primary border-b-2 border-brand-background duration-150 cursor-pointer ">
-                    Hello
+                    Help
                 </a>
                 <a className="px-4 py-1 mx-2 hover:border-brand-primary border-b-2 border-brand-background duration-150 cursor-pointer ">
-                    Hello
-                </a>
-                <a className="px-4 py-1 mx-2 hover:border-brand-primary border-b-2 border-brand-background duration-150 cursor-pointer ">
-                    Hello
+                    Statistics
                 </a>
             </div>
             <button className="text-lg font-bold border-2 bg-brand-primary px-4 py-1 rounded-lg hover:bg-brand-background hover:border-brand-background hover:-translate-y-[2px] hover:drop-shadow-lg hover:text-brand-primary duration-150  text-brand-background">
